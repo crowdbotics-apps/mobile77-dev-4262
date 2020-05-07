bf75383353b7c993b3c185fe3c30671afae64add
Crowdbotics <team@crowdbotics.com>
2020-05-07 12:24:09 +0000

    Update admin title

--- backend/mobile77_dev_4262/urls.py
+++ backend/mobile77_dev_4262/urls.py
@@ -33,9 +33,9 @@ urlpatterns = [
     path("home/", include("home.urls")),
 ]
 
-admin.site.site_header = "mobile77"
-admin.site.site_title = "mobile77 Admin Portal"
-admin.site.index_title = "mobile77 Admin"
+admin.site.site_header = "mobile777"
+admin.site.site_title = "mobile777 Admin Portal"
+admin.site.index_title = "mobile777 Admin"
 
 # swagger
 schema_view = get_schema_view(
