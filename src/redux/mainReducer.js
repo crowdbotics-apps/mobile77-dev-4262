f44b8552284de02accb129a25e1f6fe00aa8366a
Crowdbotics <team@crowdbotics.com>
2020-05-07 06:20:22 +0000

    add package blueprint-react-native-emailauth-screen

--- src/redux/mainReducer.js
+++ src/redux/mainReducer.js
@@ -6,6 +6,7 @@ import { combineReducers } from "redux";
 
 
 //@BlueprintReduxImportInsertion
+import EmailAuth3811Reducer from '../features/EmailAuth3811/redux/reducers';
 import EmailAuthReducer from '../features/EmailAuth/redux/reducers';
 
 export const combinedReducers = combineReducers({
@@ -16,6 +17,7 @@ export const combinedReducers = combineReducers({
 
 
   //@BlueprintReduxCombineInsertion
+EmailAuth3811: EmailAuth3811Reducer,
 EmailAuth: EmailAuthReducer,
 
 });
--- src/redux/mainSaga.js
+++ src/redux/mainSaga.js
@@ -2,6 +2,7 @@ import { all, takeEvery, take } from "redux-saga/effects";
 
 
 //@BlueprintReduxSagaImportInsertion
+import EmailAuth3811Saga from '../features/EmailAuth3811/redux/sagas';
 import EmailAuthSaga from '../features/EmailAuth/redux/sagas';
 
 function* helloSaga() {
@@ -15,6 +16,7 @@ export function* mainSaga() {
 
 
     //@BlueprintReduxSagaMainInsertion
+EmailAuth3811Saga,
 EmailAuthSaga,
     
   ]);
